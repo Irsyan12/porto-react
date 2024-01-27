52503ed898a7a71e2d4256bf77c9d866f94d0c52
Irsyan12 <irsyah@mhs.usk.ac.id>
2024-01-27 16:11:06 +0700

    1.5

--- src/components/NavBarMbl.jsx
+++ src/components/NavBarMbl.jsx
@@ -73,7 +73,7 @@ export default function NavBar2() {
             <Menu.Item>
               {({ active }) => (
                 <a
-                  href="#"
+                  href="#contactme"
                   className={classNames(
                     active ? "bg-gray-100 text-gray-900" : "",
                     "block px-4 py-2 text-sm"
